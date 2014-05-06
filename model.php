821991047c829732a4b349990052cf0e46fc2246
Kenta Suzuki <suzuken326@gmail.com>
2014-05-06 16:26:20 +0900

    MySQLを利用しないようにした。

--- model.php
+++ model.php
@@ -1,31 +1,17 @@
 <?php
-$app['db.pdo'] = $app->share(function ($c) {
-    $db_config = $c['db.config'];
-    $pdo = new PDO(
-        sprintf('mysql:host=%s;dbname=%s;charset=utf8', $db_config['host'], $db_config['database']),
-        $db_config['user'],
-        $db_config['password'],
-        array(PDO::ATTR_EMULATE_PREPARES => false)
-    );
-
-    return $pdo;
+$app['data.json'] = $app->share(function ($c) {
+    $filename = __DIR__ . '/data/item.json';
+    return json_decode(fread(fopen($filename, 'r'), filesize($filename)), true);
 });
 
 $app['model.all_items'] = function ($c) {
-    $stmt = $c['db.pdo']->query('SELECT id, name, photo_src FROM item');
-    $items = array();
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $items[] = $row;
-    }
-
-    return $items;
+    return $c['data.json'];
 };
 
 $app['model.item_by_id'] = $app->protect(function ($id) use ($app) {
-    $sth = $app['db.pdo']->prepare('SELECT * FROM item where id = :id');
-    $sth->bindValue(':id', $id, PDO::PARAM_INT);
-    $sth->execute();
-    $item = $sth->fetch(PDO::FETCH_ASSOC);
-
-    return $item;
+    foreach ($app['data.json'] as $d) {
+        if ($d['id'] === (int)$id) {
+            return $d;
+        }
+    }
 });
